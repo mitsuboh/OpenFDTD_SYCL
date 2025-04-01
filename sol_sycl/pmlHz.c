9b2282fe37f3d04571b2cdb9c0ee48ef05290ab1
mitsuboh <mitsuru.ikei@gmail.com>
2025-04-01 12:57:07 +0900

    Sycl version of pml completed.

--- sol_sycl/pmlHz.c
+++ sol_sycl/pmlHz.c
@@ -6,11 +6,17 @@ PML for Hz
 
 #include "ofd.h"
 
+#ifdef _ONEAPI
+#undef C	// C is used for (2.99792458e8) but <CL/sycl.hpp> refuses it
+#include "ofd_dpcpp.h"
+#endif
+
 void pmlHz(void)
 {
 	const int lx = cPML.l;
 	const int ly = cPML.l;
 
+#ifndef _ONEAPI
 	int64_t n;
 #ifdef _OPENMP
 #pragma omp parallel for
@@ -34,4 +40,56 @@ void pmlHz(void)
 
 		HZ(i, j, k) = Hzx[n] + Hzy[n];
 	}
+#else //_ONEAPI
+	const int pmlBlock = 256; 
+	sycl::range<1> updateBlock = sycl::range<1>(pmlBlock);
+	sycl::range<1> grid(CEIL(numPmlHz, pmlBlock));
+	sycl::range<1> all_grid = grid * updateBlock;
+
+	myQ.submit([&](sycl::handler& hndl) {
+		auto numPmlHz = ::numPmlHz;
+		auto fPmlHz = ::fPmlHz;
+		auto Ey = ::Ey;
+		auto Ex = ::Ex;
+		auto RYc = :: RYc;
+		auto RXc = :: RXn;
+		auto rPmlH = ::rPmlH;
+		auto rPml = ::rPml;
+		auto gPmlXc = ::gPmlXc;
+		auto gPmlYc = ::gPmlYc;
+
+		auto Hzx = ::Hzx;
+		auto Hzy = ::Hzy;
+		auto Hz = ::Hz;
+		auto Ny = ::Ny;
+		auto Nx = ::Nx;
+		auto Ni = ::Ni;
+		auto Nj = ::Nj;
+		auto Nk = ::Nk;
+		auto N0 = ::N0;
+
+		hndl.parallel_for(
+			sycl::nd_range<1>(all_grid, updateBlock),
+			[=](sycl::nd_item<1> idx) {
+				const int64_t n = idx.get_global_id(0);
+				if (n < numPmlHz){
+		const int  i = fPmlHz[n].i;
+		const int  j = fPmlHz[n].j;
+		const int  k = fPmlHz[n].k;
+		const id_t m = fPmlHz[n].m;
+
+		const real_t dey = EY(i + 1, j, k) - EY(i, j, k);
+		const real_t rx = RXc[MIN(MAX(i, 0), Nx - 1)] * rPmlH[m];
+		Hzx[n] = (Hzx[n] - (rx * dey)) / (1 + (gPmlXc[i + lx] * rPml[m]));
+
+		const real_t dex = EX(i, j + 1, k) - EX(i, j, k);
+		const real_t ry = RYc[MIN(MAX(j, 0), Ny - 1)] * rPmlH[m];
+		Hzy[n] = (Hzy[n] + (ry * dex)) / (1 + (gPmlYc[j + ly] * rPml[m]));
+
+		HZ(i, j, k) = Hzx[n] + Hzy[n];
+				}
+			});
+		});
+	myQ.wait();
+#endif // _ONEAPI
 }
