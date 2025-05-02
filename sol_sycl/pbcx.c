bb4a18c7fb0243067bd8251685ff7205efd79376
mitsuboh <mitsuru.ikei@gmail.com>
2025-05-02 05:08:28 +0000

    Pbc is offloaded.

--- sol_sycl/pbcx.c
+++ sol_sycl/pbcx.c
@@ -6,12 +6,18 @@ PBC for X boundary
 
 #include "ofd.h"
 
+#ifdef _ONEAPI
+#undef C	// C is used for (2.99792458e8) but <CL/sycl.hpp> refuses it
+#include "ofd_dpcpp.h"
+#endif
+
 void pbcx(void)
 {
 	const int id1 = -1;
 	const int id2 = 0;
 	const int id3 = Nx - 1;
 	const int id4 = Nx;
+#ifndef _ONEAPI
 	int j;
 
 	// Hy
@@ -24,7 +30,37 @@ void pbcx(void)
 		HY(id4, j, k) = HY(id2, j, k);
 	}
 	}
+#else //_ONEAPI
+	const int pbcBlock = 16;
+	sycl::range<2> updateBlock = sycl::range<2>(pbcBlock,pbcBlock);
+	sycl::range<2> grid_hy(CEIL(jMax - jMin + 1, updateBlock[0]),
+		             CEIL(kMax - kMin + 2, updateBlock[1]));
+	sycl::range<2> all_grid_y = grid_hy * updateBlock;
 
+	myQ.submit([&](sycl::handler& hndl) {
+		auto jMin = ::jMin;
+		auto kMin = ::kMin;
+		auto jMax = ::jMax;
+		auto kMax = ::kMax;
+		auto N0 = ::N0;
+		auto Ni = ::Ni;
+		auto Nj = ::Nj;
+		auto Nk = ::Nk;
+		auto Hy = ::Hy;
+		hndl.parallel_for(
+			sycl::nd_range<2>(all_grid_y, updateBlock),
+			[=](sycl::nd_item<2> idx) {
+				auto j = jMin + idx.get_global_id(0);
+				auto k = kMin - 1 + idx.get_global_id(1);
+				if((j <= jMax) && (k <= kMax)){
+					HY(id1, j, k) = HY(id3, j, k);
+					HY(id4, j, k) = HY(id2, j, k);
+				 }
+			});
+		});
+	myQ.wait();
+#endif
+#ifndef _ONEAPI
 	// Hz
 #ifdef _OPENMP
 #pragma omp parallel for
@@ -35,4 +71,32 @@ void pbcx(void)
 		HZ(id4, j, k) = HZ(id2, j, k);
 	}
 	}
+#else //_ONEAPI
+	sycl::range<2> grid_hz(CEIL(jMax - jMin + 2, updateBlock[0]),
+		             CEIL(kMax - kMin + 1, updateBlock[1]));
+	sycl::range<2> all_grid_z = grid_hz * updateBlock;
+
+	myQ.submit([&](sycl::handler& hndl) {
+		auto jMin = ::jMin;
+		auto kMin = ::kMin;
+		auto jMax = ::jMax;
+		auto kMax = ::kMax;
+		auto N0 = ::N0;
+		auto Ni = ::Ni;
+		auto Nj = ::Nj;
+		auto Nk = ::Nk;
+		auto Hz = ::Hz;
+		hndl.parallel_for(
+			sycl::nd_range<2>(all_grid_z, updateBlock),
+			[=](sycl::nd_item<2> idx) {
+				auto j = jMin - 1 + idx.get_global_id(0);
+				auto k = kMin + idx.get_global_id(1);
+				if((j <= jMax) && (k <= kMax)){
+					HZ(id1, j, k) = HZ(id3, j, k); 
+					HZ(id4, j, k) = HZ(id2, j, k);
+				 }
+			});
+		});
+	myQ.wait();
+#endif
 }
--- sol_sycl/pbcy.c
+++ sol_sycl/pbcy.c
@@ -6,12 +6,18 @@ PBC for Y boundary
 
 #include "ofd.h"
 
+#ifdef _ONEAPI
+#undef C	// C is used for (2.99792458e8) but <CL/sycl.hpp> refuses it
+#include "ofd_dpcpp.h"
+#endif
+
 void pbcy(void)
 {
 	const int id1 = -1;
 	const int id2 = 0;
 	const int id3 = Ny - 1;
 	const int id4 = Ny;
+#ifndef _ONEAPI
 	int k;
 
 	// Hz
@@ -24,7 +30,37 @@ void pbcy(void)
 		HZ(i, id4, k) = HZ(i, id2, k);
 	}
 	}
+#else //_ONEAPI
+	const int pbcBlock = 16;
+	sycl::range<2> updateBlock = sycl::range<2>(pbcBlock,pbcBlock);
+	sycl::range<2> grid_hz(CEIL(kMax - kMin + 1, updateBlock[0]),
+		             CEIL(iMax - iMin + 2, updateBlock[1]));
+	sycl::range<2> all_grid_z = grid_hz * updateBlock;
 
+	myQ.submit([&](sycl::handler& hndl) {
+		auto iMin = ::iMin;
+		auto kMin = ::kMin;
+		auto iMax = ::iMax;
+		auto kMax = ::kMax;
+		auto N0 = ::N0;
+		auto Ni = ::Ni;
+		auto Nj = ::Nj;
+		auto Nk = ::Nk;
+		auto Hz = ::Hz;
+		hndl.parallel_for(
+			sycl::nd_range<2>(all_grid_z, updateBlock),
+			[=](sycl::nd_item<2> idx) {
+				auto k = kMin + idx.get_global_id(0);
+				auto i = iMin - 1 + idx.get_global_id(1);
+				if((k <= kMax) && (i <= iMax)){
+					HZ(i, id1, k) = HZ(i, id3, k);
+					HZ(i, id4, k) = HZ(i, id2, k);
+				 }
+			});
+		});
+	myQ.wait();
+#endif
+#ifndef _ONEAPI
 	// Hx
 #ifdef _OPENMP
 #pragma omp parallel for
@@ -35,4 +71,33 @@ void pbcy(void)
 		HX(i, id4, k) = HX(i, id2, k);
 	}
 	}
+}
+#else //_ONEAPI
+	sycl::range<2> grid_hx(CEIL(kMax - kMin + 2, updateBlock[0]),
+		             CEIL(iMax - iMin + 1, updateBlock[1]));
+	sycl::range<2> all_grid_x = grid_hx * updateBlock;
+
+	myQ.submit([&](sycl::handler& hndl) {
+		auto iMin = ::iMin;
+		auto kMin = ::kMin;
+		auto iMax = ::iMax;
+		auto kMax = ::kMax;
+		auto N0 = ::N0;
+		auto Ni = ::Ni;
+		auto Nj = ::Nj;
+		auto Nk = ::Nk;
+		auto Hx = ::Hx;
+		hndl.parallel_for(
+			sycl::nd_range<2>(all_grid_x, updateBlock),
+			[=](sycl::nd_item<2> idx) {
+				auto k = kMin - 1 + idx.get_global_id(0);
+				auto i = iMin + idx.get_global_id(1);
+				if((k <= kMax) && (i <= iMax)){
+					HX(i, id1, k) = HX(i, id3, k);
+					HX(i, id4, k) = HX(i, id2, k);
+				 }
+			});
+		});
+	myQ.wait();
+#endif
 }
