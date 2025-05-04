2d47518738169fb2f2dd7c40d182a0da43f29ebb
mitsuboh <mitsuru.ikei@gmail.com>
2025-05-04 10:16:24 +0900

    Eload and vpoint are offloaded.

--- sol_sycl/eload.c
+++ sol_sycl/eload.c
@@ -7,12 +7,18 @@ E on loads (inductors)
 #include "ofd.h"
 #include "ofd_prototype.h"
 
+#ifdef _ONEAPI
+#undef C	// C is used for (2.99792458e8) but <CL/sycl.hpp> refuses it
+#include "ofd_dpcpp.h"
+#endif
+
 void eload(void)
 {
 	if (NInductor <= 0) return;
 
-	const double cdt = C * Dt;
+	const double cdt = (2.99792458e8) * Dt;
 
+#ifndef _ONEAPI
 	for (int n = 0; n < NInductor; n++) {
 		inductor_t *ptr = &Inductor[n];
 
@@ -54,4 +60,78 @@ void eload(void)
 			ptr->esum += ptr->e;
 		}
 	}
+#else	// _ONEAPI
+
+	const int block = 256;
+	sycl::range<1> updateBlock = sycl::range<1>(block);
+	sycl::range<1> grid(CEIL(NInductor, block));
+	sycl::range<1> all_grid = grid * updateBlock;
+
+	myQ.submit([&](sycl::handler& hndl) {
+		auto NInductor = ::NInductor;
+		auto Inductor = ::Inductor;
+		auto iMin = ::iMin;
+		auto jMin = ::jMin;
+		auto kMin = ::kMin;
+		auto iMax = ::iMax;
+		auto jMax = ::jMax;
+		auto kMax = ::kMax;
+		auto N0 = ::N0;
+		auto Ni = ::Ni;
+		auto Nj = ::Nj;
+		auto Nk = ::Nk;
+		auto Ex = ::Ex;
+		auto Ey = ::Ey;
+		auto Ez = ::Ez;
+		auto Hx = ::Hx;
+		auto Hy = ::Hy;
+		auto Hz = ::Hz;
+		hndl.parallel_for(
+			sycl::nd_range<1>(all_grid, updateBlock),
+			[=](sycl::nd_item<1> idx) {
+			const int64_t n = idx.get_global_id(0);
+			if( n < NInductor) {
+				inductor_t *ptr = &Inductor[n];
+				int     i = ptr->i;
+				int     j = ptr->j;
+				int     k = ptr->k;
+				double dx = ptr->dx;
+				double dy = ptr->dy;
+				double dz = ptr->dz;
+
+		if      ((ptr->dir == 'X') &&
+		         (iMin <= i) && (i <  iMax) &&
+		         (jMin <= j) && (j <= jMax) &&
+		         (kMin <= k) && (k <= kMax)) {  // MPI
+			const double roth = (HZ(i, j, k) - HZ(i,     j - 1, k    )) / dy
+			                  - (HY(i, j, k) - HY(i,     j,     k - 1)) / dz;
+			EX(i, j, k) = (real_t)(ptr->e + (cdt * roth) - (ptr->fctr * cdt * cdt * ptr->esum));
+			ptr->e = EX(i, j, k);
+			ptr->esum += ptr->e;
+		}
+		else if ((ptr->dir == 'Y') &&
+		         (iMin <= i) && (i <= iMax) &&
+		         (jMin <= j) && (j <  jMax) &&
+		         (kMin <= k) && (k <= kMax)) {  // MPI
+			const double roth = (HX(i, j, k) - HX(i,     j,     k - 1)) / dz
+			                  - (HZ(i, j, k) - HZ(i - 1, j,     k    )) / dx;
+			EY(i, j, k) = (real_t)(ptr->e + (cdt * roth) - (ptr->fctr * cdt * cdt * ptr->esum));
+			ptr->e = EY(i, j, k);
+			ptr->esum += ptr->e;
+		}
+		else if ((ptr->dir == 'Z') &&
+		         (iMin <= i) && (i <= iMax) &&
+		         (jMin <= j) && (j <= jMax) &&
+		         (kMin <= k) && (k <  kMax)) {  // MPI
+			const double roth = (HY(i, j, k) - HY(i - 1, j,     k    )) / dx
+			                  - (HX(i, j, k) - HX(i,     j - 1, k    )) / dy;
+			EZ(i, j, k) = (real_t)(ptr->e + (cdt * roth) - (ptr->fctr * cdt * cdt * ptr->esum));
+			ptr->e = EZ(i, j, k);
+			ptr->esum += ptr->e;
+		}
+			}
+			});
+		});
+	myQ.wait();
+#endif	// _ONEAPI
 }
--- sol_sycl/vpoint.c
+++ sol_sycl/vpoint.c
@@ -7,9 +7,15 @@ V waveform on points
 #include "ofd.h"
 #include "finc.h"
 
+#ifdef _ONEAPI
+#undef C	// C is used for (2.99792458e8) but <CL/sycl.hpp> refuses it
+#include "ofd_dpcpp.h"
+#endif
+
 void vpoint(int itime)
 {
 	if (NPoint <= 0) return;
+#ifndef _ONEAPI
 	real_t fi, dfi;
 
 	for (int n = 0; n < NPoint + 2; n++) {
@@ -58,4 +64,94 @@ void vpoint(int itime)
 		const int id = n * (Solver.maxiter + 1) + itime;
 		VPoint[id] = e * (-d);
 	}
+#else //_ONEAPI
+	const int PointBlock = 256;
+	sycl::range<1> block = sycl::range<1>(MIN(PointBlock, NPoint + 2));
+	sycl::range<1> grid(CEIL(NPoint + 2, block));
+	sycl::range<1> all_grid = grid * block;
+
+	myQ.submit([&](sycl::handler& hndl) {
+		auto NPoint = ::NPoint;
+		auto Point = ::Point;
+		auto VPoint = ::VPoint;
+		auto IPlanewave = ::IPlanewave;
+		auto SPlanewave = ::d_SPlanewave;
+		auto iMin = ::iMin;
+		auto iMax = ::iMax;
+		auto jMin = ::jMin;
+		auto jMax = ::jMax;
+		auto kMin = ::kMin;
+		auto kMax = ::kMax;
+		auto N0 = ::N0;
+		auto Ni = ::Ni;
+		auto Nj = ::Nj;
+		auto Nk = ::Nk;
+		auto Ex = ::Ex;
+		auto Ey = ::Ey;
+		auto Ez = ::Ez;
+		auto s_Xn = ::s_Xn;
+		auto s_Yn = ::s_Yn;
+		auto s_Zn = ::s_Zn;
+		auto s_Xc = ::s_Xc;
+		auto s_Yc = ::s_Yc;
+		auto s_Zc = ::s_Zc;
+		auto Dt = ::Dt;
+		real_t s_t = (real_t) ::Dt;
+		auto Solver = ::Solver;
+		hndl.parallel_for(
+			sycl::nd_range<1>(all_grid, block),
+			[=](sycl::nd_item<1> idx) {
+				const int64_t n = idx.get_global_id(0);
+				if (n < NPoint + 2){
+					const int i = Point[n].i;
+					const int j = Point[n].j;
+					const int k = Point[n].k;
+
+		double e = 0;
+		double d = 0;
+		real_t fi = 0;
+		real_t dfi = 0;
+		if      ((Point[n].dir == 'X') &&
+		         (iMin <= i) && (i <  iMax) &&
+		         (jMin <= j) && (j <= jMax) &&
+		         (kMin <= k) && (k <= kMax)) {  // MPI
+			e = EX(i, j, k);
+			d = Point[n].dx;
+			if (IPlanewave) {
+				const real_t t = (itime + 1) * Dt;
+				finc_s(s_Xc[i], s_Yn[j], s_Zn[k], t, SPlanewave->r0, SPlanewave->ri, SPlanewave->ei[0], SPlanewave->ai, s_t, &fi, &dfi);
+				e += fi;
+			}
+		}
+		else if ((Point[n].dir == 'Y') &&
+		         (iMin <= i) && (i <= iMax) &&
+		         (jMin <= j) && (j <  jMax) &&
+		         (kMin <= k) && (k <= kMax)) {  // MPI
+			e = EY(i, j, k);
+			d = Point[n].dy;
+			if (IPlanewave) {
+				const double t = (itime + 1) * Dt;
+				finc_s(s_Xn[i], s_Yc[j], s_Zn[k], t, SPlanewave->r0, SPlanewave->ri, SPlanewave->ei[1], SPlanewave->ai, s_t, &fi, &dfi);
+				e += fi;
+			}
+		}
+		else if ((Point[n].dir == 'Z') &&
+		         (iMin <= i) && (i <= iMax) &&
+		         (jMin <= j) && (j <= jMax) &&
+		         (kMin <= k) && (k <  kMax)) {  // MPI
+			e = EZ(i, j, k);
+			d = Point[n].dz;
+			if (IPlanewave) {
+				const double t = (itime + 1) * Dt;
+				finc_s(s_Xn[i], s_Yn[j], s_Zc[k], t, SPlanewave->r0, SPlanewave->ri, SPlanewave->ei[2], SPlanewave->ai, s_t, &fi, &dfi);
+				e += fi;
+			}
+		}
+
+		const int id = n * (Solver.maxiter + 1) + itime;
+		VPoint[id] = e * (-d);
+				}
+		});
+	});
+#endif // _ONEAPI
 }
